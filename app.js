cf26abe97ba80a55e3b680906543626b900b5fcf
les8 <leonid.skurihin@yandex.ru>
2021-09-24 10:15:40 +0300

    some improvement

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const slides = document.querySelectorAll('.slide');
-for(const slide of slides) {    
-    slide.addEventListener('click', () => {
-        clearActiveClasses();
-        slide.classList.add('active');
-    });
+function slidesPlugin(number) {
+    const slides = document.querySelectorAll('.slide');
+    slides[number].classList.add('active');
+    for(const slide of slides) {    
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        });
+    };
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        });
+    };
 };
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    });
-};
+slidesPlugin(0);
